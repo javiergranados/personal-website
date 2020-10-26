efc8d23927094f2e297605a360b8f78e3aafe813
Javier Granados <contact@jvgranados.com>
2020-10-26 01:50:46 +0100

    Hide navbar option for lg resolutions and above

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,9 @@ function Navbar() {
   return (
     <nav className="flex flex-wrap items-center justify-between p-6 border-b bg-dark border-light">
       <div className="items-center flex-shrink-0 hidden mr-6 text-white lg:flex">
-        <Logo className="w-16 ml-8 mr-2" />
+        <Link to="/">
+          <Logo className="w-16 ml-8 mr-2" />
+        </Link>
       </div>
       <div className="block lg:hidden">
         <button
@@ -43,16 +45,16 @@ function Navbar() {
           showMenu ? 'flex' : 'hidden'
         } flex-grow w-full lg:flex lg:items-center lg:w-auto lg:flex-row flex-col`}
       >
-        <Link to="/" className="navbar__button odd first">
+        <Link to="/" className="block navbar__button odd first lg:hidden">
           /home
         </Link>
         <Link to="/about" className="navbar__button even">
           /about
         </Link>
-        <Link to="/projects" className="navbar__button odd">
+        <Link to="/projects" className="navbar__button even">
           /projects
         </Link>
-        <Link to="/uses" className="navbar__button even">
+        <Link to="/uses" className="navbar__button odd">
           /uses
         </Link>
         {/* TODO:  theme button switch  */}
