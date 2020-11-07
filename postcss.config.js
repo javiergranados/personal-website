a3a4368cf427b88d8390fa68c3f3a4fa2564cfb6
Javier Granados <contact@jvgranados.com>
2020-11-07 12:29:26 +0100

    Implement purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+    defaultExtractor: content => {
+      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || []
+      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || []
+      return broadMatches.concat(innerMatches)
+    },
+  },
+]
+
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env'],
+  plugins: ['tailwindcss', process.env.NODE_ENV === 'production' ? purgecss : undefined, 'postcss-preset-env'],
 }
