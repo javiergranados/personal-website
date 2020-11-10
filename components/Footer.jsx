bb260899cd5ab4251f6864506a57eeeabfc6d9d2
Javier Granados <contact@jvgranados.com>
2020-11-10 01:48:51 +0100

    Fix mobile styles

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="pt-5 pb-4 pl-8 mt-2 border-t border-light">
+    <footer className="pt-5 pb-4 pl-8 mt-12 border-t border-light">
       {`© ${new Date().getFullYear()} Javier Granados - Web Developer`}
     </footer>
   )
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,20 +13,20 @@ function Layout({ title, children }) {
       </Head>
       <div className="flex flex-col flex-1 w-screen h-screen text-base text-light bg-dark font-regular">
         <Navbar />
-        <div className="relative flex flex-col flex-1 w-full max-w-5xl px-8 mx-auto my-0">
-          <main className="flex-1">
-            <div className="block md:hidden animate__animated animate__fadeIn">
-              <Image src="/img/cover.svg" alt="cover" width={360} height={275} loading="eager" />
-            </div>
-            <div className="hidden md:block lg:hidden animate__animated animate__fadeIn">
-              <Image src="/img/cover-md.svg" alt="cover" width={705} height={380} loading="eager" />
-            </div>
-            <div className="hidden lg:block lg:max-w-4xl animate__animated animate__fadeIn">
-              <Image src="/img/cover-lg.svg" alt="cover" width={895} height={360} loading="eager" />
-            </div>
-            <div className="text-center animate__animated animate__fadeIn md:text-left">{children}</div>
-          </main>
-        </div>
+        <main className="relative flex flex-col flex-1 w-full max-w-5xl px-8 mx-auto my-0 overflow-auto">
+          {/* <main className="flex-1"> */}
+          <div className="block md:hidden animate__animated animate__fadeIn">
+            <Image src="/img/cover.svg" alt="cover" width={360} height={275} loading="eager" />
+          </div>
+          <div className="hidden md:block lg:hidden animate__animated animate__fadeIn">
+            <Image src="/img/cover-md.svg" alt="cover" width={705} height={380} loading="eager" />
+          </div>
+          <div className="hidden lg:block lg:max-w-4xl animate__animated animate__fadeIn">
+            <Image src="/img/cover-lg.svg" alt="cover" width={895} height={360} loading="eager" />
+          </div>
+          <div className="order-2 text-center animate__animated animate__fadeIn md:text-left">{children}</div>
+          {/* </main> */}
+        </main>
         <Footer />
       </div>
     </>
