367b2cd552497fe42f57b0d23ac026f8a2062c84
Javier Granados <contact@jvgranados.com>
2020-11-11 12:35:46 +0100

    Add new image for meta tags

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -6,6 +6,7 @@ function Meta() {
       {/* HTML Meta Tags */}
       <title>Javier Granados</title>
       <link rel="shortcut icon" href="/img/favicon.png" />
+      <meta charset="UTF-8" />
       <meta name="description" content="Welcome to my personal website! 🚀" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
@@ -14,15 +15,16 @@ function Meta() {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Javier Granados" />
       <meta property="og:description" content="Welcome to my personal website! 🚀" />
-      <meta property="og:image" content="https://jvgranados.com/img/logo.svg" />
+      <meta property="og:image" content="https://jvgranados.com/img/logo.png" />
 
       {/* Twitter Meta Tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="jvgranados.com" />
+      <meta name="twitter:card" content="player" />
       <meta property="twitter:url" content="https://jvgranados.com/" />
       <meta name="twitter:title" content="Javier Granados" />
       <meta name="twitter:description" content="Welcome to my personal website! 🚀" />
-      <meta name="twitter:image" content="https://jvgranados.com/img/logo.svg" />
+      <meta name="twitter:site" content="@jvgranados_" />
+      <meta name="twitter:image" content="https://jvgranados.com/img/logo.png" />
+      <meta name="twitter:creator" content="@jvgranados_" />
     </Head>
   )
 }
