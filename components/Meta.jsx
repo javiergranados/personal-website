e25a4e419e3850887f3ec91806815a38f71fbc93
Javier Granados <contact@jvgranados.com>
2020-11-11 12:13:00 +0100

    Add og meta tags

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -3,6 +3,11 @@ import Head from 'next/head'
 function Meta() {
   return (
     <Head>
+      <meta property="og:url" content="https://jvgranados.com/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Javier Granados" />
+      <meta property="og:description" content="Welcome to my personal website! 🚀" />
+      <meta property="og:image" content="https://jvgranados.com/img/logo.svg" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="shortcut icon" href="/img/favicon.png" />
       <title>Javier Granados</title>
