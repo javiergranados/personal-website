fde2f43ff00c5b7a23928ed10b70fb80036ca55b
Javier Granados <hola@javiergranados.dev>
2021-09-22 02:09:23 +0200

    Update about page and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="flex pt-5 pb-4 border-t border-lightpl-0 md:pl-8">
-      <span className="w-full text-center md:text-left">{`© ${new Date().getFullYear()} Javier Granados - Web Developer`}</span>
+      <span className="w-full text-center md:text-left">{`© ${new Date().getFullYear()} Javier Granados - Software Engineer`}</span>
     </footer>
   )
 }
--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ function AboutPage() {
   return (
     <>
       <p className="text-4xl font-semi">
-        Software Engineer | Husband | Father of 2 | Rock Climbing | Tea lover | Fan of Nintendo | Fantasy books
+        Software Engineer | Husband | Father of 3 | Climbing lover | Tea drinker | Fan of Nintendo | Fantasy books
         enthusiast
       </p>
     </>
