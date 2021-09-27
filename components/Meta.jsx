c277cf88a1a5d22400fcceeba1cc2aaffd7c5add
Javier Granados <hola@javiergranados.dev>
2021-09-27 23:06:49 +0200

    Modify open graph meta tags

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -10,19 +10,19 @@ function Meta() {
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
       {/* Open Graph Meta Tags */}
-      <meta property="og:url" content="https://javiergranados.dev/" />
+      <meta property="og:url" content="https://www.javiergranados.dev/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Javier Granados" />
       <meta property="og:description" content="Welcome to my personal website! 🚀" />
-      <meta property="og:image" content="https://javiergranados.dev/img/logo.png" />
+      <meta property="og:image" content="https://www.javiergranados.dev/img/logo.png" />
 
       {/* Twitter Meta Tags */}
       <meta name="twitter:card" content="summary" />
-      <meta property="twitter:url" content="https://javiergranados.dev/" />
+      <meta property="twitter:url" content="https://www.javiergranados.dev/" />
       <meta name="twitter:title" content="Javier Granados" />
       <meta name="twitter:description" content="Welcome to my personal website! 🚀" />
       <meta name="twitter:site" content="@jvgranados_" />
-      <meta name="twitter:image" content="https://javiergranados.dev/img/logo.png" />
+      <meta name="twitter:image" content="https://www.javiergranados.dev/img/logo.png" />
       <meta name="twitter:creator" content="@jvgranados_" />
     </Head>
   )
