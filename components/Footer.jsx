c47fa132f758085645a958a8270883e31bbb4a56
Javier Granados <hola@javiergranados.dev>
2022-07-06 11:16:04 +0200

    Modify footer and about page texts

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="flex pt-5 pb-4 border-t border-lightpl-0 md:pl-8">
-      <span className="w-full text-center md:text-left">{`© ${new Date().getFullYear()} Javier Granados - Software Engineer`}</span>
+      <span className="w-full text-center md:text-left">{`© ${new Date().getFullYear()} Javier Granados - Frontend Developer`}</span>
     </footer>
   )
 }
--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ function AboutPage() {
   return (
     <>
       <p className="text-4xl font-semi">
-        Software Engineer | Husband | Father of 3 | Casual rock climber | Tea drinker | OST lover | Fantasy books
+        Frontend Developer | Husband | Father of 3 | Casual rock climber | Tea drinker | OST lover | Fantasy books
         enthusiast
       </p>
     </>
